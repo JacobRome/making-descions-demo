d2d868a397aaf4bb27492394e633ea68156a9768
Jacob rome <ejrome17@gmail.com>
2023-03-01 14:02:00 -0700

    inital message

---
+++ code-intro/WedLab.js
@@ -0,0 +1,10 @@
+let obiwanAttack = 25
+let anakinAttack = 35
+
+if (obiwanAttack > anakinAttack) {
+    console.log('Obiwan has a better attack than Anakin.')
+} else if (anakinAttack > obiwanAttack) {
+    console.log('Anakin has a better attack than Obiwan.')
+} else {
+    console.log('Anakin and Obiwan have the same attack.')
+}
